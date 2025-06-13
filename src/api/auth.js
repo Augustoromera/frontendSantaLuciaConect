4c43c12855cfa1dcd8795531affcc49ee10b88ee
Augusto Romera <newsromers@gmail.com>
2025-06-13 13:17:18 -0300

    se hicieron cambios en api x2

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from './axios';
 
-export const API = 'https://localhost:8080/api';
+export const API = 'https://santaluciaback.netlify.app/api';
 
 export const registerRequest = (user) => axios.post(`/register`, user);
 export const loginRequest = (user) => axios.post(`/login`, user);
