778c6dd9808877ccc8897cacbeea2585531aed17
Marcos Brandan <marcosbrandan681@gmail.com>
2025-08-08 10:31:48 -0300

    merge aplicados

--- src/api/pruebaApi.js
+++ src/api/pruebaApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const pruebaApi = axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: 'https://backendsantaluciaconnect.onrender.com/',
 });
 
 export default pruebaApi;
